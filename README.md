1aa7b9272268e0beb8dbf2a5e6024f0411051aeb
Klaus Hartl <klaus.hartl@gmail.com>
2023-07-13 10:19:54 +0200

    Automate installation completely

--- README.md
+++ README.md
@@ -5,17 +5,5 @@
 ## Installation
 
 ```bash
-./install.sh
-```
-
-## Setup in a project
-
-```bash
-cat << EOF > lefthook.yml
----
-remote:
-  git_url: https://github.com/carhartl/lefthook-config
-EOF
-lefthook install
-echo lefthook.yml >> .git/info/exclude
+curl -Lks https://raw.githubusercontent.com/carhartl/lefthook-config/main/install.sh | /bin/bash
 ```
--- install.sh
+++ install.sh
@@ -10,4 +10,15 @@ brew install \
     tflint \
     yamllint
 go install mvdan.cc/sh/v3/cmd/shfmt@v3.7.0
-cp commitlint.config.js "$HOME/.commitlint.config.js"
+
+cat << EOF > lefthook.yml
+---
+remote:
+  git_url: https://github.com/carhartl/lefthook-config
+EOF
+cp commitlint.config.js .commitlint.config.js
+
+echo lefthook.yml >> .git/info/exclude
+echo .commitlint.config.js >> .git/info/exclude
+
+lefthook install
