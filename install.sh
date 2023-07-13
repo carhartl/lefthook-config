b4b543dd88c2c0d6b88722da6b79a40c1b5405af
Klaus Hartl <klaus.hartl@gmail.com>
2023-07-13 10:25:11 +0200

    Run shell script through shfmt

--- install.sh
+++ install.sh
@@ -1,29 +1,29 @@
 #!/bin/sh
 
 if ! [ -d .git ]; then
-    echo "Must run in a git repository!"
-    exit 1
+	echo "Must run in a git repository!"
+	exit 1
 fi
 
 brew install \
-    golangci-lint \
-    hadolint \
-    lefthook \
-    prettier \
-    shellcheck \
-    terraform \
-    tflint \
-    yamllint
+	golangci-lint \
+	hadolint \
+	lefthook \
+	prettier \
+	shellcheck \
+	terraform \
+	tflint \
+	yamllint
 go install mvdan.cc/sh/v3/cmd/shfmt@v3.7.0
 
-cat << EOF > lefthook.yml
+cat <<EOF >lefthook.yml
 ---
 remote:
   git_url: https://github.com/carhartl/lefthook-config
 EOF
 cp commitlint.config.js .commitlint.config.js
 
-echo lefthook.yml >> .git/info/exclude
-echo .commitlint.config.js >> .git/info/exclude
+echo lefthook.yml >>.git/info/exclude
+echo .commitlint.config.js >>.git/info/exclude
 
 lefthook install
