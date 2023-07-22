f0221e9e8ed42a9d261f36d03ab24d20bc32be25
Klaus Hartl <klaus.hartl@gmail.com>
2023-07-22 09:04:31 +0200

    Make scoped Git hook configurations

--- install.sh
+++ install.sh
@@ -1,25 +1,51 @@
 #!/bin/sh
 
+_install_go() {
+	brew install golangci-lint
+	go install github.com/rakyll/gotest@latest
+}
+
+_install_terraform() {
+	brew install terraform tflint
+}
+
 if ! [ -d .git ]; then
 	echo "Must run in a git repository!"
 	exit 1
 fi
 
 brew install \
-	golangci-lint \
 	hadolint \
 	lefthook \
 	prettier \
 	shellcheck \
-	terraform \
-	tflint \
 	yamllint
 go install mvdan.cc/sh/v3/cmd/shfmt@v3.7.0
+
+BUNDLE_NAME="${1:-lefthook}"
+
+case "$BUNDLE_NAME" in
+"lefthook")
+	_install_go
+	_install_terraform
+	;;
+"go")
+	_install_go
+	;;
+"terraform")
+	_install_terraform
+	;;
+*)
+	_fail "Unknown bundle name, must be one of \"go\", \"terraform\"."
+	exit 1
+	;;
+esac
 
 cat <<EOF >lefthook.yml
 ---
 remote:
   git_url: https://github.com/carhartl/lefthook-config
+  config: $BUNDLE_NAME.yml
 EOF
 echo lefthook.yml >>.git/info/exclude
 
