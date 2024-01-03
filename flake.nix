7f48d60e1040fd62851680fa4c04cefb561ba302
Klaus Hartl <klaus.hartl@gmail.com>
2024-01-03 21:04:54 +0100

    Use Terraform version 1.5.7 in flake

--- flake.nix
+++ flake.nix
@@ -2,8 +2,10 @@
   description = "Lefthook setup";
   inputs.nixpkgs.url = "github:nixos/nixpkgs";
   inputs.flake-utils.url = "github:numtide/flake-utils";
+  # Fetched via https://lazamar.co.uk/nix-versions/?channel=nixpkgs-unstable&package=terraform
+  inputs.terraform_1_5_7-nixpkgs.url = "github:nixos/nixpkgs/4ab8a3de296914f3b631121e9ce3884f1d34e1e5";
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, terraform_1_5_7-nixpkgs }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { 
@@ -12,17 +14,17 @@
         };
         lefthook-bin =  pkgs.symlinkJoin {
           name = "lefthook-env";
-          paths = with pkgs; [
-            actionlint
-            golangci-lint
-            gotest
-            hadolint
-            nodePackages.prettier
-            shellcheck
-            shfmt
-            terraform
-            tflint
-            yamllint
+          paths = [
+            pkgs.actionlint
+            pkgs.golangci-lint
+            pkgs.gotest
+            pkgs.hadolint
+            pkgs.nodePackages.prettier
+            pkgs.shellcheck
+            pkgs.shfmt
+            terraform_1_5_7-nixpkgs.legacyPackages.${system}.terraform
+            pkgs.tflint
+            pkgs.yamllint
           ];
           # symlinkJoin can't handle symlinked dirs and nodePackages,
           # thus symlink ./bin -> ./lib/node_modules/.bin/.
